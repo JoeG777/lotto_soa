43226287df0e1f537f198396e7f7c99eb5c2f7cb
JoeG777 <jojo.klettern@googlemail.com>
2023-01-12 11:27:25 +0100

    add example calls

---
+++ example_calls/draw_and_show_results.py
@@ -0,0 +1,45 @@
+import requests
+
+import example_calls.register_bet as register_bet
+
+# Precondition: empty database, running services and all processes
+# Steps:
+    # 1. register one/multiple bets -> winning_classe needs to be null
+    # 2. trigger draw on drawing_service
+        # - return a valid drawing result
+        # - publish message to rabbitmq
+        # - message consumed by customer_service
+        # - view_results shows winning_class without "null" - corresponds to matching with result
+
+
+BASE_URI = "http://localhost:8080"
+
+def show_empty_database():
+    available_bets_in_db = register_bet.check_for_registered_bet()
+    #assert available_bets_in_db.content == b'{"message":"Something went wrong! - No entries available"}'
+    return available_bets_in_db.content
+
+def register_bet_local():
+    return register_bet.register_bet().content
+
+def trigger_draw():
+    
+    test_draw_response = requests.get(f"{BASE_URI}/trigger_draw")
+    return test_draw_response
+
+def show_results():
+    now_available_results = register_bet.check_for_registered_bet()
+    return now_available_results
+
+if __name__ == '__main__':
+    print(f"\n>>> Content in Database:\n")
+    print(show_empty_database())
+
+    print(f"\n>>> Register a bet in the database:\n")
+    print(register_bet_local())
+
+    print(f"\n>>> Trigger a Lotto Draw:\n")
+    print(trigger_draw().content)
+
+    print(f"\n>>> Show results of draw:\n")
+    print(show_results().content)
---
+++ example_calls/register_bet.py
@@ -0,0 +1,25 @@
+import requests
+
+BASE_URI = "http://localhost:8085"
+
+
+def register_bet():
+    test_bet = {
+        "winning_numbers": [1, 2, 3, 4, 5, 6],
+        "super_number": [0],
+        "user": "123"
+    }
+
+    test_created_object = requests.post(f"{BASE_URI}/add_bet", json=test_bet)
+    return test_created_object
+
+def check_for_registered_bet():
+
+    test_results = requests.get(f"{BASE_URI}/show_results")
+    return test_results
+
+
+if __name__ == "__main__":
+    print(register_bet().content)
+    print("Registered Bets: ")
+    print(check_for_registered_bet().content)
