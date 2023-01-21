e146709a6df50eda90ba04f66ee05fa3d0f30e35
JoeG777 <jojo.klettern@googlemail.com>
2023-01-21 12:14:42 +0100

    add docs doc

--- drawing_service/src/draw/application/app.py
+++ drawing_service/src/draw/application/app.py
@@ -5,7 +5,14 @@ from src.draw.application.models import LottoDraw, LottoDrawEvent
 from src.draw.application.drawer import Drawer
 from src.draw.application.publisher import Publisher
 
-api = FastAPI()
+api = FastAPI(
+    title="Drawing Service",
+    description="",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 @api.get("/index")
 def index():
